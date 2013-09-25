38b2d91d844fda38593fc1ad23d7e98df40c4fa9
Steven Barragan <me@steven.mx>
2013-09-25 17:46:14 -0500

    Update dummy app to be rails 4 compatible

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Dummy::Application.config.secret_token = 'f85ba1b68938f6dc63b681a3ff9a538b06305688095bbf0983aee0dd3294851bdd34318cd66daaf0d1e0075773591a80eb59da7b03e97fb742058b8c7130982d'
+Dummy::Application.config.secret_key_base = 'f85ba1b68938f6dc63b681a3ff9a538b06305688095bbf0983aee0dd3294851bdd34318cd66daaf0d1e0075773591a80eb59da7b03e97fb742058b8c7130982d'
